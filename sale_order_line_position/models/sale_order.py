OCA/sale-reporting

sale_order_line_position: split recompute_positions

sale_order_line_position/models/sale_order.py
@@ -26,7 +26,10 @@ class SaleOrder(models.Model):
         for sale in self:
             if sale.locked_positions:
                 continue
-            lines = sale.order_line.filtered(lambda l: not l.display_type)
-            lines.sorted(key=lambda x: (x.sequence, x.id))
-            for position, line in enumerate(lines, start=1):
-                line.position = position
+            sale._recompute_positions()
+
+    def _recompute_positions(self):
+        lines = self.order_line.filtered(lambda l: not l.display_type)
+        lines.sorted(key=lambda x: (x.sequence, x.id))
+        for position, line in enumerate(lines, start=1):
+            line.position = position

brain-tec/sale-reporting-1

sale_order_line_position/models/sale_order.py
@@ -26,7 +26,10 @@ class SaleOrder(models.Model):
         for sale in self:
             if sale.locked_positions or sale.company_id.disable_sale_position_recompute:
                 continue
-            lines = sale.order_line.filtered(lambda line: not line.display_type)
-            lines.sorted(key=lambda x: (x.sequence, x.id))
-            for position, line in enumerate(lines, start=1):
-                line.position = position
+            sale._recompute_positions()
+
+    def _recompute_positions(self):
+        lines = self.order_line.filtered(lambda line: not line.display_type)
+        lines.sorted(key=lambda x: (x.sequence, x.id))
+        for position, line in enumerate(lines, start=1):
+            line.position = position